6191f6a1bd1daeb044bcdad35a58ee27c0ed2e6c
Akassh <100897807+akashmuraliNEW@users.noreply.github.com>
2023-10-29 12:24:58 +0530

    Update yt_dl.py

--- plugins/ExraMods/yt_dl.py
+++ plugins/ExraMods/yt_dl.py
@@ -12,7 +12,8 @@ from info import MUSIC_CHAT
 
 @Client.on_message(filters.command(['song', 's']) & ~filters.private & ~filters.channel)
 async def song(client, message):
-   if MUSIC_CHAT == message.chat.id:
+    chat_id = message.chat.id
+   if MUSIC_CHAT == chat_id:
     user_id = message.from_user.id 
     user_name = message.from_user.first_name 
     rpk = "["+user_name+"](tg://user?id="+str(user_id)+")"
