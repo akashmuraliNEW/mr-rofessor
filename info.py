caa784f4a47f370f630c216bb2f63ea02f475c32
Akassh <100897807+akashmuraliNEW@users.noreply.github.com>
2024-06-16 08:48:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ def is_enabled(value, default):
 
 # Bot information
 PORT = environ.get("PORT", "8080")
-WEBHOOK = bool(environ.get("WEBHOOK", True)) # for web support on/off
+WEB_SUPPORT = bool(environ.get("WEBHOOK", True)) # for web support on/off
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
