c44e803c3fb2474e6de0257dd21d46042b6dd053
Akassh <100897807+akashmuraliNEW@users.noreply.github.com>
2024-06-16 09:14:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -58,7 +58,7 @@ class Bot(Client):
             await app.setup()
             bind_address = "0.0.0.0"
             await web.TCPSite(app, bind_address, PORT).start()
-        logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
+            logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
         #started_telethroid() # installation Telethroid Library   
         if LOG_CHANNEL:
             try:
